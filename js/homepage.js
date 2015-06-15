5c6633fbfabdbe68885043fac02ba573878536f6
davidzxc12 <davidzxc12@hotmail.com>
2015-06-15 12:29:08 +0800

    add_prev&next

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,18 @@
-$(document).ready(function(){
-	var owl=$('.owl-carousel');
+$(document).ready(function() {
+ 
+  var owl = $("#owl-example");
+ 
   owl.owlCarousel({
-  	items:4,
+      items : 3, //10 items above 1000px browser width
   });
+ 
+  // Custom Navigation Events
+  $(".next").hover(function(){
+    owl.trigger('next.owl.carousel');
+  })
+  $(".prev").hover(function(){
+    owl.trigger('prev.owl.carousel');
+  })
+  
+ 
 });
