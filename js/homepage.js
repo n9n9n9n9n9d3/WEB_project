4c92ac11b1afaf8b1695fb930a5df3ed0977fdda
davidzxc12 <davidzxc12@hotmail.com>
2015-06-19 20:50:30 +0800

    initialize

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,23 @@
 //Parse initialize
 Parse.initialize("zq26lP7dyXL0SyvJ1SOyDjUwn27O9Wxa3eFehdi7", "nrMNihVPIxOwlls7NJD6CtYsfpnxRiP0xXsIGP6T");
 
+
+window.fbAsyncInit = function() {
+    Parse.FacebookUtils.init({
+      appId      : '1461518597495814',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "http://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 //query function
 
 var queryCat = function(depend, owl) {
